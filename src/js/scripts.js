28a548df47d1336f4f9b797f73a2de0d4ecd33e6
AndrewGibson27 <atgibson27@gmail.com>
2016-10-19 21:59:08 -0400

    let's use those xss filters

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,8 +2,15 @@
   'use strict';
 
   var formHandler = function(){
-    var form = document.getElementById('form');
-      //amountsList = document.getElementById('amounts-list');
+    var form = document.getElementById('form'),
+      $frequencyButtons = $('.form__frequency-button'),
+      amountLabels = document.querySelectorAll('.form__amount-label'),
+      amountsList = {
+        annual: ['35', '60', '150', '250', '500'],
+        monthly: ['4', '9', '17', '31', '63'],
+        once: ['35', '60', '150', '250', '500']
+      },
+      currentFrequency = 'monthly';
 
     var init = function(){
       _bindEvents();
@@ -13,8 +20,30 @@
       e.preventDefault();
     };
 
+    var _updateAmounts = function(frequency){
+      var newAmounts = amountsList[frequency];
+
+
+
+      currentFrequency = frequency;
+    };
+
     var _bindEvents = function(){
       form.onsubmit = _handleSubmit;
+
+      $frequencyButtons.each(function(i){
+        var $this = $(this);
+
+        $this.click(function(e){
+          var frequency = $this.attr('data-frequency');
+
+          if (frequency === currentFrequency) {
+            return false;
+          }
+
+          _updateAmounts(frequency);
+        });
+      });
     };
 
     return {
