71a13ea5275cfb2cdf8b806f05a8de48fd6fc66b
AndrewGibson27 <atgibson27@gmail.com>
2016-10-19 21:18:38 -0400

    add skeleton of formHandler module

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,27 @@
+(function(){
+  'use strict';
 
+  var formHandler = function(){
+    var form = document.getElementById('form');
+      //amountsList = document.getElementById('amounts-list');
+
+    var init = function(){
+      _bindEvents();
+    };
+
+    var _handleSubmit = function(e){
+      e.preventDefault();
+    };
+
+    var _bindEvents = function(){
+      form.onsubmit = _handleSubmit;
+    };
+
+    return {
+      init: init
+    };
+  };
+
+  var formModule = formHandler();
+  formModule.init();
+})();
